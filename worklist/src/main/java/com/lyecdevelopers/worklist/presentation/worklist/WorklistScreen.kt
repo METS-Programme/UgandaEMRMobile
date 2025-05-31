f9b95f248547788cfe16f60b2c8ca4904d86822b
jabahum <jabajnan8@gmail.com>
2025-05-31 19:45:18 +0300

    add scaffold

--- worklist/src/main/java/com/lyecdevelopers/worklist/presentation/worklist/WorklistScreen.kt
+++ worklist/src/main/java/com/lyecdevelopers/worklist/presentation/worklist/WorklistScreen.kt
@@ -1,10 +1,25 @@
 package com.lyecdevelopers.worklist.presentation.worklist
 
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 
-// Worklist module
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
 @Composable
-fun WorklistScreen(onSyncClick: () -> Unit, onSettingsClick: () -> Unit) {
-    Text("This is the Worklist Screen")
+fun WorklistScreen(
+    onSyncClick: () -> Unit,
+    onSettingsClick: () -> Unit
+) {
+    Scaffold { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+
+        }
+    }
 }
