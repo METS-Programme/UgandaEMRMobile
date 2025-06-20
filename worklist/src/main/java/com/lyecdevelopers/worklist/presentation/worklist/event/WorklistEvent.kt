1cf29ec333a2f4883868270d44ea49e05fde9ef9
jabahum <jabajnan8@gmail.com>
2025-06-20 17:53:51 +0300

    fix filter by status bug

--- worklist/src/main/java/com/lyecdevelopers/worklist/presentation/worklist/event/WorklistEvent.kt
+++ worklist/src/main/java/com/lyecdevelopers/worklist/presentation/worklist/event/WorklistEvent.kt
@@ -5,7 +5,7 @@ import com.lyecdevelopers.core.model.VisitStatus
 sealed class WorklistEvent {
     data class OnNameFilterChanged(val name: String) : WorklistEvent()
     data class OnGenderFilterChanged(val gender: String?) : WorklistEvent()
-    data class OnStatusFilterChanged(val status: VisitStatus) : WorklistEvent()
+    data class OnStatusFilterChanged(val status: VisitStatus?) : WorklistEvent()
     object OnClearFilters : WorklistEvent()
     object OnRefresh : WorklistEvent()
     data class OnPatientSelected(val patientId: String) : WorklistEvent()
