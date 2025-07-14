67396ee3a0b67365dd43a7a91b7068edabe1eb37
jabahum <jabajnan8@gmail.com>
2025-07-14 12:17:56 +0300

    Refactor: Prepend "/rest/v1/" to API endpoints and update savePatient
    This commit updates the API endpoint paths across `AuthApi.kt` and `FormApi.kt` by prepending `/rest/v1/` to ensure consistency with the new API versioning scheme.
    
    Additionally, the `savePatient` endpoint in `FormApi.kt` has been modified:
    - The HTTP method changed from `POST` to `PUT`.
    - The endpoint path now includes the patient's UUID: `fhir2/R4/Patient/{uuid}`.
    - A new `@Path("uuid") patientId: String` parameter has been added to the method signature.

--- core/src/main/java/com/lyecdevelopers/core/data/remote/FormApi.kt
+++ core/src/main/java/com/lyecdevelopers/core/data/remote/FormApi.kt
@@ -14,69 +14,71 @@ import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 
 interface FormApi {
     // form
-    @GET("form")
+    @GET("rest/v1/form")
     suspend fun getForms(
         @Query("v") view: String = "full",
     ): Response<FormsListResponse>
 
-    @GET("o3/forms/{formId}")
+    @GET("rest/v1/o3/forms/{formId}")
     suspend fun loadFormByUuid(
         @Path("formId") formId: String,
         @Query("v") view: String = "full",
     ): Response<o3Form>
 
-    @GET("form")
+    @GET("rest/v1/form")
     suspend fun filterForms(
         @Query("q") query: String,
         @Query("v") view: String = "full",
     ): Response<FormsListResponse>
 
-    @GET("o3/forms/{formId}")
+    @GET("rest/v1/o3/forms/{formId}")
     suspend fun getFormByUuid(@Path("formId") formId: String): Response<o3Form>
 
     // cohort
-    @GET("cohort")
+    @GET("rest/v1/cohort")
     suspend fun getCohorts(@Query("v") view: String = "full"): Response<CohortListResponse>
 
     // orders
-    @GET("ordertype")
+    @GET("rest/v1/ordertype")
     suspend fun getOrderTypes(): Response<OrderTypeListResponse>
 
     // encounters
-    @GET("encountertype")
+    @GET("rest/v1/encountertype")
     suspend fun getEncounterTypes(): Response<EncounterTypeListResponse>
 
     // patientIdentifiers
-    @GET("patientidentifiertype")
+    @GET("rest/v1/patientidentifiertype")
     suspend fun getPatientIdentifiers(): Response<IdentifierListResponse>
 
     // personattributetype
-    @GET("personattributetype")
+    @GET("rest/v1/personattributetype")
     suspend fun getPersonAttributeTypes(): Response<PersonAttributeTypeListResponse>
 
     // conditions
-    @GET("ugandaemrreports/concepts/conditions")
+    @GET("rest/v1/ugandaemrreports/concepts/conditions")
     suspend fun getConditions(): Response<Any>
 
     // data definition
-    @POST("ugandaemrreports/dataDefinition")
+    @POST("rest/v1/ugandaemrreports/dataDefinition")
     suspend fun generateDataDefinition(
         @Body payload: DataDefinition,
     ): Response<ResponseBody>
 
 
     // save encounter
-    @POST("encounter")
+    @POST("rest/v1/encounter")
     suspend fun saveEncounter(@Body payload: Any): Response<Any>
 
-    @POST("fhir2/Patient")
+    @PUT("fhir2/R4/Patient/{uuid}")
     suspend fun savePatient(
+        @Path("uuid") patientId: String,
         @Body patient: RequestBody,
     ): Response<Unit>
 
